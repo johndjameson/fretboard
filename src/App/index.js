96fe4357434b7451c8eff5b8d22c07c2dbce836d
John D. Jameson <thejohnjameson@gmail.com>
2018-11-11 22:43:43 -0600

    Implement partial 88 notes

--- src/App/index.js
+++ src/App/index.js
@@ -1,25 +1,22 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
+import notes from 'data/piano-notes'
 import './styles.css'
 
 class App extends Component {
   render () {
     return (
-      <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className='App-link'
-            href='https://reactjs.org'
-            target='_blank'
-            rel='noopener noreferrer'>
-            Learn React
-          </a>
-        </header>
-      </div>
+      <table>
+        <tbody>
+          {notes.map(note => (
+            <tr key={note.position}>
+              <td>{note.position}</td>
+              <td>{note.note}</td>
+              <td>{note.id}</td>
+              <td>{note.frequency}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     )
   }
 }

4515f950c6973fbe460a61c9db952371f3771907
John D. Jameson <thejohnjameson@gmail.com>
2023-04-14 21:38:17 -0500

    Add initial fretboard with tuning select

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,107 @@
-import React from 'react';
-import logo from './logo.svg';
+import classNames from 'classnames';
+import { useState } from 'react';
+
 import './App.css';
 
-function App() {
+type Note =
+  | 'A'
+  | 'A♯/B♭'
+  | 'B'
+  | 'C'
+  | 'C♯/D♭'
+  | 'D'
+  | 'D♯/E♭'
+  | 'E'
+  | 'F'
+  | 'F♯/G♭'
+  | 'G'
+  | 'G♯/A♭';
+
+const NOTES: readonly Note[] = [
+  'A',
+  'A♯/B♭',
+  'B',
+  'C',
+  'C♯/D♭',
+  'D',
+  'D♯/E♭',
+  'E',
+  'F',
+  'F♯/G♭',
+  'G',
+  'G♯/A♭',
+] as const;
+
+interface NoteProps {
+  note: Note;
+}
+
+function GuitarNote({ note }: NoteProps) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <span
+      className={classNames('guitar-note', {
+        'guitar-note--accidental': note.includes('/'),
+      })}
+    >
+      {note.replace('/', '\n')}
+    </span>
+  );
+}
+
+interface GuitarStringProps {
+  note: Note;
+  frets: number;
+}
+
+function GuitarString({ frets = 24, note }: GuitarStringProps) {
+  // Number of frets plus open string
+  const fretNumbers = new Array(frets + 1).fill(null).map((_v, i) => i);
+
+  const startingNoteIndex = NOTES.indexOf(note);
+
+  return (
+    <div
+      className="guitar-string"
+      style={{ '--guitar-frets': frets } as React.CSSProperties}
+    >
+      {fretNumbers.map((fret) => (
+        <GuitarNote note={NOTES[(startingNoteIndex + fret) % NOTES.length]} />
+      ))}
     </div>
+  );
+}
+
+const TUNINGS = {
+  'E Standard': ['E', 'B', 'G', 'D', 'A', 'E'],
+  'Drop D': ['E', 'B', 'G', 'D', 'A', 'D'],
+  'D Standard': ['D', 'A', 'F', 'C', 'G', 'D'],
+  'Drop C': ['D', 'A', 'F', 'C', 'G', 'C'],
+  'C Standard': ['C', 'G', 'D♯/E♭', 'A♯/B♭', 'F', 'C'],
+} as const;
+
+function App() {
+  const [tuning, setTuning] = useState<keyof typeof TUNINGS>('E Standard');
+
+  return (
+    <>
+      <select
+        onChange={(e) => {
+          setTuning(e.target.value as typeof tuning);
+        }}
+      >
+        {Object.keys(TUNINGS).map((tuning) => (
+          <option key={tuning} value={tuning}>
+            {tuning}
+          </option>
+        ))}
+      </select>
+
+      <div className="guitar">
+        {TUNINGS[tuning].map((stringNote) => (
+          <GuitarString frets={24} note={stringNote} />
+        ))}
+      </div>
+    </>
   );
 }
 
